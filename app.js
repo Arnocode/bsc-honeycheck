09b87640e2382319902ebc1a2b0872e19096f1b0
Arnonymous <arno@arnonymous.xyzm>
2021-08-23 20:14:00 +0200

    removed unneeded code and updated readme

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -17,7 +15,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.disable('x-powered-by');
 
 app.use('/honeycheck', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
